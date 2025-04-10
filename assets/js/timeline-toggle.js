7591b0eba87025baf57528883896e25c917b932d
Andre Neto <netoandre.neto@gmail.com>
2025-04-10 10:15:22 +0100

    modulated css

--- assets/js/timeline-toggle.js
+++ assets/js/timeline-toggle.js
@@ -1,13 +1,3 @@
-/*
- * Filename: timeline-toggle.js
- * Path: /home/netoa/Desktop/ags-neto/js
- * Created Date: Sunday April 6th 2025
- * Author: Andre Neto
- * -----
- * Last Modified: Tue Apr 08 2025
- * Modified By: Andre Neto
- */
-
 function toggleTimeline(sectionId) {
   const timeline = document.getElementById(`timeline-${sectionId}`);
   const arrow = document.getElementById(`toggle-arrow-${sectionId}`);
