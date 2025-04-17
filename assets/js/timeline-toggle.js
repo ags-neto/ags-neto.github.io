f67adcc56715886f0bf0fd71fc0be09f13631fe7
Andre Neto <netoandre.neto@gmail.com>
2025-04-17 15:18:26 +0100

    arrow animation

--- assets/js/timeline-toggle.js
+++ assets/js/timeline-toggle.js
@@ -1,12 +1,4 @@
 function toggleTimeline(sectionId) {
   const timeline = document.getElementById(`timeline-${sectionId}`);
-  const arrow = document.getElementById(`toggle-arrow-${sectionId}`);
-
   timeline.classList.toggle("collapsed");
-
-  if (timeline.classList.contains("collapsed")) {
-    arrow.innerHTML = "&#9663;";
-  } else {
-    arrow.innerHTML = "&#9653;";
-  }
 }
